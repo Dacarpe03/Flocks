9d1abc7280dc1f36f7e4794f2853a3e844a1e68a
Dacarpe03 <45000161+Dacarpe03@users.noreply.github.com>
2020-03-15 13:20:33 +0100

    Add Bird relation to Main class

--- Main.py
+++ Main.py
@@ -1,8 +1,8 @@
 import arcade
 from Model.Bird import Bird
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 2000
+SCREEN_HEIGHT = 1000
 
 
 class MyGame(arcade.Window):
@@ -10,7 +10,8 @@ class MyGame(arcade.Window):
 
     def __init__(self, width, height):
         super().__init__(width, height)
-
+        self.bird = Bird([0, 0], [0.5, 0.5])
+        self.radiusBird = 5
         arcade.set_background_color(arcade.color.AMAZON)
 
     def setup(self):
@@ -21,17 +22,22 @@ class MyGame(arcade.Window):
         """ Render the screen. """
         arcade.start_render()
         # Your drawing code goes here
+        self.drawBird(self.bird)
 
     def update(self, delta_time):
         """ All the logic to move, and the game logic goes here. """
+        self.bird.move()
         pass
 
+    def drawBird(self, bird):
+        x = bird.position[0]
+        y = bird.position[1]
+        arcade.draw_circle_filled(x, y, self.radiusBird, arcade.color.YELLOW)
 
 def main():
     game = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT)
     game.setup()
     arcade.run()
-    bird = Bird([0, 0], [10, 10])
 
 
 
--- Model/Bird.py
+++ Model/Bird.py
@@ -7,4 +7,4 @@ class Bird:
 
     def move(self):
         self.position[0] += self.vector[0]
-        self.positoin[1] += self.vector[1]
+        self.position[1] += self.vector[1]
